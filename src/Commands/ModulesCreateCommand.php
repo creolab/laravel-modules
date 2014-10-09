6704ccb853edf9022fb95a90fe3167b273d8d3dc
Mohamed Meabed <mo.meabed@gmail.com>
2014-10-09 06:01:04 +0200

    Adding commands directory and .gitkeep to the folders

--- src/Commands/ModulesCreateCommand.php
+++ src/Commands/ModulesCreateCommand.php
@@ -58,14 +58,30 @@ class ModulesCreateCommand extends AbstractCommand {
 			$routes = '<?php' . PHP_EOL;
 			$this->app['files']->put($modulePath . '/routes.php', $routes);
 
-			// Create some resource directories
-			$this->app['files']->makeDirectory($modulePath . '/assets', 0755);
-			$this->app['files']->makeDirectory($modulePath . '/config', 0755);
-			$this->app['files']->makeDirectory($modulePath . '/controllers', 0755);
-			$this->app['files']->makeDirectory($modulePath . '/lang', 0755);
-			$this->app['files']->makeDirectory($modulePath . '/models', 0755);
-			$this->app['files']->makeDirectory($modulePath . '/migrations', 0755);
-			$this->app['files']->makeDirectory($modulePath . '/views', 0755);
+            // Create some resource directories
+            $this->app['files']->makeDirectory($modulePath . '/commands', 0755);
+            $this->app['files']->put($modulePath . '/commands/.gitkeep', '');
+
+            $this->app['files']->makeDirectory($modulePath . '/assets', 0755);
+            $this->app['files']->put($modulePath . '/assets/.gitkeep', '');
+
+            $this->app['files']->makeDirectory($modulePath . '/config', 0755);
+            $this->app['files']->put($modulePath . '/config/.gitkeep', '');
+
+            $this->app['files']->makeDirectory($modulePath . '/controllers', 0755);
+            $this->app['files']->put($modulePath . '/controllers/.gitkeep', '');
+
+            $this->app['files']->makeDirectory($modulePath . '/lang', 0755);
+            $this->app['files']->put($modulePath . '/lang/.gitkeep', '');
+
+            $this->app['files']->makeDirectory($modulePath . '/models', 0755);
+            $this->app['files']->put($modulePath . '/models/.gitkeep', '');
+
+            $this->app['files']->makeDirectory($modulePath . '/migrations', 0755);
+            $this->app['files']->put($modulePath . '/migrations/.gitkeep', '');
+
+            $this->app['files']->makeDirectory($modulePath . '/views', 0755);
+            $this->app['files']->put($modulePath . '/views/.gitkeep', '');
 
 			// Autoload classes
 			$this->dumpAutoload();
